ddafa3c69d916145f1246e27460607d53fd658d9
weizongwei <weizongwei5@gmail.com>
2018-04-12 16:27:54 +0800

    [需求]:{转发功能，fixbug错误的省份名}

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,26 @@
 // 载入配置文件
 var config = require('../../config');
 
+
+var ST_P_KEY = 'key_provinceindex';
+function setPreSelectProv() {
+
+  try {
+    wx.setStorageSync(ST_P_KEY, provn)
+  } catch (e) {
+  }
+}
+
+function getPreSelectProv() {
+  var st_p_v = wx.getStorageSync(ST_P_KEY)
+  if (st_p_v !== null || st_p_v !== undefined) {
+    return st_p_v;
+  }
+  else {
+    return 0;
+  }
+}
+
 function formatterDateTime() {
   var date = new Date()
   var month = date.getMonth() + 1
@@ -22,9 +42,9 @@ function formatterDateTime() {
   return datetime;
 }
 
-var that=null;
+var that = null;
 
-function requestGas(provinve_name){
+function requestGas(provinve_name) {
 
   wx.request({
     url: "https://route.showapi.com/138-46",
@@ -65,12 +85,12 @@ function requestGas(provinve_name){
 }
 
 
-//data begin
+//PAGE begin
 
 Page({
   data: { // 参与页面渲染的数据
-    provincearray: ['上海', '北京', '江苏', '浙江', '安徽','天津', '重庆','香港','澳门','河北','山西','内蒙','辽宁','吉林','黑龙江','福建','江西','山东','河南','湖北','湖南','广东','广西','海南','四川','贵州','云南','西藏','陕西','甘肃','青海','宁夏','新疆'],
-    provinceindex:0,
+    provincearray: ['上海', '北京', '江苏', '浙江', '安徽', '天津', '重庆', '河北', '山西', '内蒙古', '辽宁', '吉林', '黑龙江', '福建', '江西', '山东', '河南', '湖北', '湖南', '广东', '广西', '海南', '四川', '贵州', '云南', '西藏', '陕西', '甘肃', '青海', '宁夏', '新疆'],
+    provinceindex: 0,
     showLoading: true,
     p95: null,
     p92: null,
@@ -85,15 +105,35 @@ Page({
     console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
       provinceindex: e.detail.value,
-      showloading:true
+      showloading: true
     })
-    requestGas(that.data.provincearray[that.data.provinceindex])
+
+    setPreSelectProv(this.data.provinceindex);
+
+    requestGas(this.data.provincearray[this.data.provinceindex])
   },
   onLoad: function () {
 
     that = this;
+    this.setData({ provinceindex: getPreSelectProv() })
 
     requestGas(that.data.provincearray[that.data.provinceindex])
 
+  },
+  onShareAppMessage: function (res) {
+    if (res.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(res.target)
+    }
+    return {
+      title: '纯粹油价，不扯虚的',
+      path: '/index',
+      success: function (res) {
+        // 转发成功
+      },
+      fail: function (res) {
+        // 转发失败
+      }
+    }
   }
 })
